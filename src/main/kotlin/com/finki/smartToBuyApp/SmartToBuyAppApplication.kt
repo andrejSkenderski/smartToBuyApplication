788564dac7524627dd65588d704eed7d8afdb474
Andrej <adnrej.skenderski@gmail.com>
2023-02-27 19:09:18 +0100

    Initial commit

---
+++ src/main/kotlin/com/finki/smartToBuyApp/SmartToBuyAppApplication.kt
@@ -0,0 +1,11 @@
+package com.finki.smartToBuyApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartToBuyAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmartToBuyAppApplication>(*args)
+}
